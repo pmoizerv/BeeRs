d81b019be8d6056bcd66c7d09e5d29aeddc0f882
pmoizerv <panagiotis.zervakis@su.se>
2023-06-20 16:33:34 +0200

    Shiny app works!

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -3,6 +3,7 @@
 
 usethis::use_package("shiny")
 usethis::use_package("utils")
+use
 
 
 
@@ -15,33 +16,47 @@ ui <- shiny::fluidPage(
   # Sidebar with a slider input for number of bins
   shiny::sidebarLayout(
     shiny::sidebarPanel(
-      shiny::selectInput("beer_csv", "Beer production by", choices = NULL, multiple = T)
-#      sliderInput("Year",
-#                   "Year:",
-#                   min = 2008,
-#                   max = 2019,
+      shiny::sliderInput("Year",
+                   "Year:",
+                   min = 2008,
+                   max = 2019,
 #                   step = 1,
-#                   sep = ",",
-#                   value = 2008),
-
-    ),
+                   sep = "",
+                   value = c(2008,2019)),
+  shiny::submitButton()
+),
 
     # Specifies what to put in the main panel
-    shiny::mainPanel(
+    shiny::mainPanel(plotOutput("plot_output",width="900px",height="700px"))
 
     )
   )
-)
 
 
 
 server <- function(input, output, session) {
 
-  get_data <- shiny::reactive({
-    shiny::req()
-    utils::read.csv(input$my_csv$datapath, header = TRUE)
 
-  })
+
+  output$plot_output <- renderPlot({beers_dat |>
+      tidyr::pivot_longer(4:6, values_to = "Volume", names_to = "Purpose") |>
+      dplyr::filter(year %in% input$Year[1]:input$Year[2],
+                    Purpose %in% COLUMN,
+                    state != "total") |>
+      dplyr::group_by(state, state_full) |>
+      dplyr::summarise(AVG = almost(Volume, N = NumberBeers)) |>
+      dplyr::ungroup() |>
+
+      #dplyr::mutate(AVG = ifelse(AVG == NA, 0, as.numeric(AVG))) |>
+      dplyr::mutate(Total = sum(AVG, na.rm = T),
+                    Percent = (AVG/Total) * 100) |>
+
+      ggplot2::ggplot(mapping = ggplot2::aes(map_id = state_full))+
+      ggplot2::geom_map(mapping = ggplot2::aes(fill = Percent), map = states_map) +
+      ggplot2::expand_limits(x = states_map$long, y = states_map$lat)})
+
+
+
 
   # output$distPlot <- renderPlot({
   #    # generate bins based on input$bins from ui.R
